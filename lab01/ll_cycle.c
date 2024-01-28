ba525abfa2978df6b0567205b197fc63f0e74de2
Filtee <filtee0812@gmail.com>
2024-01-28 19:36:00 +0800

    Lab01: Finished!

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    node *tortoise = head, *hare = head;
+
+    do {
+        if (hare == NULL || hare->next == NULL) {
+            return 0;
+        }
+
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+
+    } while (tortoise != hare);
+    
+    return 1;
 }
